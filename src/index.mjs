4719940b6f2b469adf8e26654b079398f2aa3c37
MarcLawrenceKing <124873963+MarcLawrenceKing@users.noreply.github.com>
2025-01-03 16:42:33 +0800

    post and put requests

--- src/index.mjs
+++ src/index.mjs
@@ -2,12 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
   {id: 1, username: "marc" ,display: "Marc"},
   {id: 2, username: "lawrence" ,display: "Lawrence"},
-  {id: 3, username: "kuky" ,display: "Kuky"}
+  {id: 3, username: "w" ,display: "w"},
 ]
 app.get('/', (request, response) =>{
   response.status(201).send({msg:"Hello World!"});
@@ -20,14 +22,20 @@ app.get('/api/users', (req, res) => {
 
   // destructure 
   const { query: {filter,value},} = req;
-
-  if(!filter && !value) //if no filter, just return everything
-    return res.send(mockUsers);
+    
   if(filter && value)
-    return res.send(mockUsers.filter((user) => user.user[filter].includes(value)))
+    return res.send(mockUsers.filter((user) => user[filter].includes(value)))
 
+  return res.send(mockUsers);
   
 }) // /api prefix is good practice
+
+app.post('/api/users', (req,res)=> {
+  const {body} = req;
+  const newUser = {id:mockUsers[mockUsers.length-1].id+1, ...body};
+  mockUsers.push(newUser);
+  return res.status(201).send(newUser);
+});
 
 // u can have one or more params to present dynamic data
 app.get('/api/users/:id', (req,res) => {
@@ -39,9 +47,10 @@ app.get('/api/users/:id', (req,res) => {
     return res.status(400).send({ msg: "BAD REQUEST!"});
 
   //find the user inside mockUsers array
-  const findUser = mockUsers.find((user) => user.id === parsedId)
+  const findUser = mockUsers.find((user) => user.id === parsedId);
   //if not found
-  if (!findUser) return res.sendStatus(404);
+  if (!findUser) 
+    return res.sendStatus(404);
   // if found
   return res.send(findUser)
 });
@@ -50,8 +59,22 @@ app.get('/api/products', (req,res) => {
   res.send([
     {id: 123, name: "sugar", price: 10}
   ])
-})
+});
 
+app.put('/api/users/:id', (req, res) => {
+  const {body, params: {id},} = req;
+  const parsedId = parseInt(id);
+  if(isNaN(parsedId)) 
+    return res.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1)
+    return res.sendStatus(404);
+
+  mockUsers[findUserIndex] = {id: parsedId, ...body};
+  return res.sendStatus(200);
+});
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`)
 })
